c138dc2bdb5c7e45c04fbee96f52506dab1a7d17
Безуглов Сергей <Besuglov.S@Gmail.com>
2014-02-22 08:39:21 +0400

    First Touch

---
+++ Schedule.DomainClasses/Main/Faculty.cs
@@ -0,0 +1,22 @@
+﻿namespace Schedule.DomainClasses.Main
+{
+    public class Faculty
+    {
+        public Faculty()
+        {
+            
+        }
+
+        public Faculty(string name, string letter, int sortingOrder)
+        {
+            Name = name;
+            Letter = letter;
+            SortingOrder = sortingOrder;
+        }
+
+        public int FacultyId { get; set; }
+        public string Name { get; set; }
+        public string Letter { get; set; }
+        public int SortingOrder { get; set; }
+    }
+}
